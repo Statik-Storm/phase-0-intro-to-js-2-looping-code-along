559b942b67946cc418aff4bcb2342d737becd5bc
Statik-Storm <statikstorm@outlook.com>
2024-11-09 01:53:33 -0800

    create myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+    var messages = [];
+    for (var i = 0; i<names.length;i++) {
+        var message ='Thank you, ' + names[i] + ', for the wonderful surprise gift!';
+        messages.push(message);
+    }
+return messages;
+}
+
+function countDown(number) {
+    while(number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
